9e5b5683ed9a09520775df26ca6c61f578bb9ed2
edersonmangueira <ederson.mangueira@hotmail.com>
2019-02-28 19:05:12 -0300

    Criando a lógica do Jogador

--- PedraPapelTesouraSemIf/src/br/com/jogosemif/PedraPapelTesoura.java
+++ PedraPapelTesouraSemIf/src/br/com/jogosemif/PedraPapelTesoura.java
@@ -1,9 +1,36 @@
 package br.com.jogosemif;
 
+import java.util.Scanner;
+
 public class PedraPapelTesoura {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Integer jogador;
+		Scanner mao = new Scanner(System.in);
+		System.out.println("_____Pedra Papel Tesoura______");
+		System.out.println("______________________________");
+		System.out.println("1 - Pedra");
+		System.out.println("2 - Papel");
+		System.out.println("3 - Tesoura");
+		System.out.println("Escolha qual objeto deseja jogar");
+
+		jogador = mao.nextInt();
+		
+		switch (jogador) {
+		case 1:
+			System.out.println("Jogador escolheu Pedra");
+			break;
+		case 2:
+			System.out.println("Jogador escolheu Papel");
+			break;
+		case 3:
+			System.out.println("Jogador escolheu Tesoura");
+			break;
+		default:
+			System.out.println("Opçao Invalida");
+			break;
+		}
 
 	}
 
